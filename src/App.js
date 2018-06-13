270abbe3f8fde9318af3a4a4f442a778dca457e3
Jesus Bossa <jesusbossa@protonmail.com>
2018-06-13 11:27:34 -0500

    Register finished

--- src/App.js
+++ src/App.js
@@ -23,18 +23,18 @@ class App extends Component {
         <div>
 
           <div className="container">
-            <div className="row justify-content-center">
+            {/* <div className="row justify-content-center">
               <div className="col-md-4 mt-3">
                 <div className="card">
                   <div className="card-header">
                     <h3 className="card-title">Hola</h3>
                   </div>
                   <div className="card-body">
-                    <h4 className="card-text">Esto es una prueba</h4>
+                    <h4 className="card-text">test</h4>
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
             <div className="row">
               <Registro/>
             </div>
--- src/components/registro/registro.js
+++ src/components/registro/registro.js
@@ -1,44 +1,46 @@
 import React, { Component } from 'react';
-import $  from  'jquery' ;
-import dt from  'datatables.net' ;
+import $ from 'jquery';
+import './registro.css'
+// eslint-disable-next-line
+import dt from 'datatables.net';
 
 const DataInfo = [
-  [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
-  [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
-  [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
-  [ "Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060" ],
-  [ "Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700" ],
-  [ "Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000" ],
-  [ "Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500" ],
-  [ "Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900" ],
-  [ "Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500" ],
-  [ "Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600" ],
-  [ "Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560" ],
-  [ "Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
-  [ "Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600" ],
-  [ "Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500" ],
-  [ "Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750" ],
-  [ "Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500" ],
-  [ "Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000" ],
-  [ "Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500" ],
-  [ "Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000" ],
-  [ "Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500" ],
-  [ "Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000" ],
-  [ "Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000" ],
-  [ "Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450" ],
-  [ "Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600" ],
-  [ "Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000" ],
-  [ "Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575" ],
-  [ "Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650" ],
-  [ "Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850" ],
-  [ "Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000" ],
-  [ "Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000" ],
-  [ "Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400" ],
-  [ "Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500" ],
-  [ "Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000" ],
-  [ "Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500" ],
-  [ "Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050" ],
-  [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
+  ["Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800"],
+  ["Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750"],
+  ["Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000"],
+  ["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060"],
+  ["Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700"],
+  ["Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000"],
+  ["Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500"],
+  ["Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900"],
+  ["Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500"],
+  ["Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600"],
+  ["Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560"],
+  ["Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000"],
+  ["Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600"],
+  ["Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500"],
+  ["Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750"],
+  ["Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500"],
+  ["Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000"],
+  ["Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500"],
+  ["Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000"],
+  ["Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500"],
+  ["Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000"],
+  ["Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000"],
+  ["Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450"],
+  ["Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600"],
+  ["Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000"],
+  ["Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575"],
+  ["Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650"],
+  ["Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850"],
+  ["Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000"],
+  ["Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000"],
+  ["Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400"],
+  ["Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500"],
+  ["Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000"],
+  ["Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500"],
+  ["Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050"],
+  ["Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675"]
 ]
 
 class registro extends Component {
@@ -51,22 +53,37 @@ class registro extends Component {
 
     //LLenar la tabla de datos
     $(document).ready(() => {
-      $('#example').DataTable({ 
+      $('#example').DataTable({
         data: DataInfo,
         columns: [
-          { title: 'Name' },
-          { title: 'Position' },
-          { title: 'Office' },
-          { title: 'Extn.' },
-          { title: 'Start date' },
-          { title: 'Salary' }
+          { title: 'Nombre' },
+          { title: 'Posición' },
+          { title: 'Oficina' },
+          { title: 'Extención' },
+          { title: 'Fecha inicio' },
+          { title: 'Salario' }
         ]
       })
+      // $('.dataTables_paginate').find('.paginate_button').addClass('btn')
+      // $('#example_wrapper').find('.dataTables_length').addClass('inline-block')
+      $('body').find('#example_wrapper').addClass('dataTables_wrapper container-fluid dt-bootstrap4 row justify-content-center')
+      $('#example_wrapper').find('.dataTables_length').addClass('input-group col-12 col-lg-4')
+      $('#xample_wrapper').find('.dataTables_length').html(`
+        <label>Show <select name="example_length" aria-controls="example" class=""><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label>
+      `)
+      $('#example_wrapper').find('.dataTables_filter').addClass('col-12 col-lg-4')
+      $('#xample_wrapper').find('.dataTables_filter').html(`
+      <label>Buscar:
+        <input type="search" class="" placeholder="" aria-controls="example">
+      </label>
+      `)
+      // $('.dataTables_length').find('select').addClass('form-control')
     })
+
     return (
       <div>
         <h3>Registro</h3>
-        <table id="example" className="table table-striped table-bordered" width="100%">
+        <table id="example" className="table table-hover col-12 .table-responsive" >
 
         </table>
       </div>
